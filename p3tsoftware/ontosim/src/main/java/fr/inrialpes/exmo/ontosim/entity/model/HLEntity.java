a12af4b5eb359c732a65c34c29bd7727434941fe
dpignataro2 <d.pignataro2@studenti.uniba.it>
2017-10-26 10:26:48 +0200

    prova

--- p3tsoftware/ontosim/src/main/java/fr/inrialpes/exmo/ontosim/entity/model/HLEntity.java
+++
@@ -1,8 +0,0 @@
-package fr.inrialpes.exmo.ontosim.entity.model;
-
-import fr.inrialpes.exmo.ontowrap.HeavyLoadedOntology;
-
-public interface HLEntity<E> extends Entity<E> {
-    public HeavyLoadedOntology<E> getOntology();
-    
-}
--- p3tsoftware/ontosim/src/main/java/fr/inrialpes/exmo/ontosim/entity/model/HLIndividual.java
+++
@@ -1,8 +0,0 @@
-package fr.inrialpes.exmo.ontosim.entity.model;
-
-import java.util.Set;
-
-public interface HLIndividual<E> extends HLEntity<E> {
-
-    public Set<HLClass<E>> getClasses(int local, int asserted, int named);
-}
